d658530ba229f4fb6119330e2dd658faa073134d
axel-sanon <axel.sanon@carbon-it.com>
2022-12-08 15:35:08 +0100

    feat: validator on todo creat  model will help returning status code

---
+++ CarbonTodo.Api/Validation/ValidationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CarbonTodo.Api.Validation.Todo;
+using FluentValidation.AspNetCore;
+
+namespace CarbonTodo.Api.Validation
+{
+    public static class ValidationExtensions
+    {
+        public static void AddValidators(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation(config =>
+            {
+                config.DisableDataAnnotationsValidation = true; 
+            
+            });
+            services.AddTodoValidators();
+        }
+    }
+}
