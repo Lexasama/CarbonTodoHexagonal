138f2f2edc6952794a79b9fbc1b7ad88351157c0
axel-sanon <axel.sanon@carbon-it.com>
2022-12-08 14:09:18 +0100

    feat: US can list of todo

---
+++ CarbonTodo.Domain.Tests/Services/TodoServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using CarbonTodo.Domain.Repositories;
+using CarbonTodo.Domain.Services;
+using Moq;
+
+namespace CarbonTodo.Domain.Tests.Services
+{
+    public class TodoServiceTests
+    {
+        private readonly Mock<ITodoRepository> _mockTodoRepository;
+        private readonly TodoService sut;
+        
+
+        public TodoServiceTests()
+        {
+            _mockTodoRepository = new Mock<ITodoRepository>();
+            sut = new TodoService(_mockTodoRepository.Object);
+        }
+
+        public async Task FindAll_should_return_list()
+        {
+            await sut.FindAll();
+            _mockTodoRepository.Verify(repo => repo.GetAll(), Times.Once);
+            _mockTodoRepository.VerifyNoOtherCalls();
+        }
+    }
+}
